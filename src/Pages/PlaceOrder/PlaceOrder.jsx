21a38918d7723da4756a56112bc5f3e263e8b4a8
Mahesh Ghule <maheshghule64@gmail.com>
2026-04-16 14:09:44 +0530

    Update PlaceOrder.jsx

--- src/Pages/PlaceOrder/PlaceOrder.jsx
+++ src/Pages/PlaceOrder/PlaceOrder.jsx
@@ -60,17 +60,17 @@ const PlaceOrder = () => {
         // let value = e.target.value;
 
          let name = e.target.name;
-        let value = data.name;
+        let value = "";
         
 
         if(name === "firstName" || name === "lastName" || name === "street" || name === "city" || name === "state" ||
              name === "country"){
                const regex = /^[A-Za-z\s]+$/;
-               if(regex.test(e.target.value)) value += e.target.value;
+               if(regex.test(e.target.value)) value = e.target.value;
         }
         else{
             const regex = /^[0-9]+$/;
-            if(regex.test(e.target.value)) value += e.target.value;
+            if(regex.test(e.target.value)) value = e.target.value;
         }
 
         setData(data=>({...data, [name]:value}));
